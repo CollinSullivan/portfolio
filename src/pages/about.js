06bd3a93c82448cc70b0b42c2a619aa28210d768
Collin Sullivan <sullic6@cs.washington.edu>
2023-02-09 10:21:39 -0500

    Added image plugin, also added some photos to the site.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
             <p>This is a page about me!</p>
+            <StaticImage
+                alt="A Husky from the University of Washington, just like me!"
+                src="https://images.seattletimes.com/wp-content/uploads/2018/03/dubs1.jpg?d=960x609"
+            />
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>Home page for my awesome portfolio site (in progress)</p>
+            <StaticImage
+                alt="Seattle, Washington"
+                src="../images/seattle.jpg"
+            />
         </Layout>
     )
 }
