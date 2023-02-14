d514574f3696f2771e23974c48141cc3afd15658
Collin Sullivan <sullic6@cs.washington.edu>
2023-02-14 18:44:58 -0500

    Fixed gatsby prod compatibility

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
-import RepositoryList from '../../components/RepositoryList'
+import RepositoryList from '../../components/gatsby-RepositoryList'
 
 const ProjectPage = ( { data }) => {
     return (
