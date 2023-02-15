2bf602fd160ddd7eb749575136bc50d32fe68563
Collin Sullivan <sullic6@cs.washington.edu>
2023-02-15 15:27:56 -0500

    Changed gatsby-node.js process to onPreBootstrap from onPostBuild. This should work better for my needs. Also updated it to give the repo access level as well. Ideally I figure out a way to not have to keep committing all these index.mdx files?

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,50 +2,52 @@ const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
 
-exports.onPostBuild = async () => {
-    try {
-        // Make the API call to get the repositories
-        const response = await axios.get(process.env.GATSBY_API_URL + 'projects?membership=true&min_access_level=30', {
-            headers: {
-                'PRIVATE-TOKEN': process.env.GATSBY_GITLAB_ACCESS_TOKEN
-            }
-        })
+exports.onPreBootstrap = async () => {
+    let currentAccessLevel = 30
+    while(currentAccessLevel < 60) {
+        try {
+            // Make the API call to get the repositories
+            const response = await axios.get(process.env.GATSBY_API_URL + 'projects?membership=true&min_access_level=' + currentAccessLevel, {
+                headers: {
+                    'PRIVATE-TOKEN': process.env.GATSBY_GITLAB_ACCESS_TOKEN
+                }
+            })
 
-        // Loop through each repository and create an .mdx file
-        response.data.forEach(repo => {
-            const repoName = repo.name
-            const repoDirPath = path.resolve(`./src/pages/projects/${repoName}`)
-            const repoFullPath = path.resolve(`./src/pages/projects/${repoName}/index.mdx`)
-            const repoSlug = repoName.replace(/\s+/g, '-').toLowerCase()
-            let repoAccessLevel
-            switch (repo.access_level) {
-                case 10:
-                  repoAccessLevel = 'Guest access';
-                  break;
-                case 20:
-                  repoAccessLevel = 'Reporter access';
-                  break;
-                case 30:
-                  repoAccessLevel = 'Developer access';
-                  break;
-                case 40:
-                  repoAccessLevel = 'Maintainer access';
-                  break;
-                case 50:
-                  repoAccessLevel = 'Owner access';
-                  break;
-                default:
-                  repoAccessLevel = 'Unknown access level';
-                  break;
-              }
-            // Create the file and write the content
-            // Uncomment the check below if you don't want overwrites every time you build the site
-            // if (fs.existsSync(repoPath)) {
-            //     console.log(`Skipping ${repo.name} file already exists`)
-            //     return
-            // }
+            // Loop through each repository and create an .mdx file
+            response.data.forEach(repo => {
+                const repoName = repo.name
+                const repoDirPath = path.resolve(`./src/pages/projects/${repoName}`)
+                const repoFullPath = path.resolve(`./src/pages/projects/${repoName}/index.mdx`)
+                const repoSlug = repoName.replace(/\s+/g, '-').toLowerCase()
+                let repoAccessLevel
+                switch (currentAccessLevel) {
+                    case 10:
+                    repoAccessLevel = 'Guest access';
+                    break;
+                    case 20:
+                    repoAccessLevel = 'Reporter access';
+                    break;
+                    case 30:
+                    repoAccessLevel = 'Developer access';
+                    break;
+                    case 40:
+                    repoAccessLevel = 'Maintainer access';
+                    break;
+                    case 50:
+                    repoAccessLevel = 'Owner access';
+                    break;
+                    default:
+                    repoAccessLevel = repo.access_level;
+                    break;
+                }
+                // Create the file and write the content
+                // Uncomment the check below if you don't want overwrites every time you build the site
+                // if (fs.existsSync(repoPath)) {
+                //     console.log(`Skipping ${repo.name} file already exists`)
+                //     return
+                // }
 
-            const content = `---
+                const content = `---
 title: "${repoName}"
 date: "${repo.last_activity_at}"
 slug: "${repoSlug}"
@@ -54,13 +56,15 @@ created: "${repo.created_at}"
 ---
 ${repo.description}
 `
-            fs.mkdirSync(repoDirPath, {recursive: true})
-            fs.writeFileSync(repoFullPath, content)
-            console.log(`Generated ${repo.name} - ${repoFullPath}`)
-        })
+                fs.mkdirSync(repoDirPath, {recursive: true})
+                fs.writeFileSync(repoFullPath, content)
+                console.log(`Generated ${repo.name} - ${repoFullPath}`)
+            })
 
-        console.log(`Generated .mdx files for ${response.data.length} repositories.`)
-    } catch (error) {
-        console.error('Error generating .mdx files:', error)
+            console.log(`Generated .mdx files for ${response.data.length} repositories.`)
+        } catch (error) {
+            console.error('Error generating .mdx files:', error)
+        }
+        currentAccessLevel = currentAccessLevel + 10
     }
 }
